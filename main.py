d3524aa500d79c3f85783574cdd0e720ca56f937
soy Joselaine <soyjoselaine@gmail.com>
2026-03-11 12:46:27 -0400

    Para leer la estructura del archivo

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+
+# Para abrir el archivo JSON
+f = open("data/iris.json")
+data = json.load(f)
+f.close()
+
+df = pd.DataFrame(data)
+
+print("Número de registros:", len(df))
+print("Columnas:", df.columns)
+print(df.head())
